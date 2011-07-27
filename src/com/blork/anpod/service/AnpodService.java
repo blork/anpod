1c5c530ebbe5727d984ed6314643bd06f89462e3
Sam Oakley <sam@blork.co.uk>
2011-07-27 18:39:29 +0100

    Rewritten service to use new code

--- src/com/blork/anpod/service/AnpodService.java
+++ src/com/blork/anpod/service/AnpodService.java
@@ -1,23 +1,149 @@
 package com.blork.anpod.service;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONException;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
+
+import com.blork.anpod.R;
+import com.blork.anpod.activity.HomeActivity;
+import com.blork.anpod.model.Picture;
+import com.blork.anpod.model.PictureFactory;
+import com.blork.anpod.util.Utils;
 
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class AnpodService.
  */
-public class AnpodService extends Service {
+public class AnpodService extends Service implements Runnable{
+
+	private NotificationManager notificationManager;
+	private boolean notify;
+	
+	static int INFO = 1;
+	static int ERROR = 2;
+	static int RUNNING = 3;
 
 	/* (non-Javadoc)
 	 * @see android.app.Service#onBind(android.content.Intent)
 	 */
 	@Override
 	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		return START_STICKY;
+	}
+
+	@Override
+	public void onCreate() {
+
+		if (!Utils.isDataEnabled(this)) {
+			finish();
+			return;
+		}
+
+		notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		notify = prefs.getBoolean("notifications_enabled", true);
+		boolean updates = prefs.getBoolean("updates_enabled", true);
+		boolean wifi_only = prefs.getBoolean("wifi", false);
+
+		if (!updates) {
+			finish();
+			return;
+		}
+
+
+		if (Utils.isNetworkConnected(this)) {
+
+			if (wifi_only && !Utils.isWiFiConnected(this)) {
+				finish();
+				return;
+			} 
+
+			new Thread(this).start();
+		}
+	}
+
+	@Override
+	public void run() {
+
+		if (notify) {
+			int icon = android.R.drawable.stat_notify_sync;
+			Notification notification = new Notification(icon, "Checking for new APOD", System.currentTimeMillis());
+			Intent syncIntent = new Intent(this, HomeActivity.class);
+			PendingIntent contentIntent = PendingIntent.getActivity(this, 0, syncIntent, 0);
+
+			notification.setLatestEventInfo(this, "Checking for new APOD", "Checking for new Astronomy Picture of the Day.", contentIntent);
+			notification.flags = Notification.FLAG_ONGOING_EVENT^Notification.FLAG_NO_CLEAR;	
+			notificationManager.notify(RUNNING, notification);
+		}
+
+		List<Picture> pictures = new ArrayList<Picture>();
+
+		try {
+			pictures.addAll(PictureFactory.load());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		if (pictures.isEmpty()) {
+			finish();
+			return;
+		}
+
+		int count = PictureFactory.saveAll(this, pictures);
+
+		if (count == 0) {
+			finish();
+			return;
+		}
+
+		Picture newPicture = pictures.get(0);
+
+		if(notify){
+			int icon = R.drawable.notify;
+			Notification notification = new Notification(icon, "New Picture!", System.currentTimeMillis());
+			notification.flags = Notification.FLAG_AUTO_CANCEL;
+			Intent notificationIntent = new Intent(this, HomeActivity.class); 
+			PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+			notification.setLatestEventInfo(
+					this,
+					"New Astronomy Pic of the Day",
+					newPicture.title, 
+					contentIntent
+			);
+			notificationManager.cancelAll();
+			notificationManager.notify(INFO, notification); 
+		}
+	}
+
+	private void finish() {
+		notificationManager.cancel(RUNNING);
+		stopSelf();
+	}
 }
