df496854d64d9c5bee46ddad70627aa3dc86009f
Sam Oakley <sam@blork.co.uk>
2011-12-20 19:13:20 +0000

    Updated for new version of ActionBarSherlock.

--- src/com/blork/anpod/activity/fragments/DetailsFragment.java
+++ src/com/blork/anpod/activity/fragments/DetailsFragment.java
@@ -84,7 +84,7 @@ abstract class DetailsFragment extends Fragment {
 				//				getActivity().getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE);
 				//				getActivity().getSupportActionBar().setTitle(picture.title);
 				//				getActivity().getSupportActionBar().setSubtitle(picture.credit);
-				getActivity().getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+				getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
 			}
 
 			BitmapFactory.Options decodeOptions = new BitmapFactory.Options();
